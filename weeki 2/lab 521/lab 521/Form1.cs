fac512850256c4f0e8a7a64774e8ad24a691dd19
eleneD <darchiaelene1@gmail.com>
2019-04-10 12:37:40 +0400

    week 2
    lab521

---
+++ weeki 2/lab 521/lab 521/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_521
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] nishnebi = { 109, 67, 34, 96, 34, 12, 34, 56, 35, 24 };
+            student studentobject -new student();
+            label1.Text = studentobject.sashualoAritmetikuli(nishnebi).ToString();
+        }
+    }
+}
