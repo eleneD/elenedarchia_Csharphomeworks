2687f77288cf54fa5f98a3014ba2f1f998e4a6eb
Irakli12 <Irakli12@users.noreply.github.com>
2019-04-10 11:29:24 +0400

    week 1
    lan5.1.1 lab5.1.2

---
+++ WEEK1/lab 5.1.1/lab 5.1.1/tvitfrinavi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_5._1._1
+{
+    class tvitfrinavi
+    {
+        private double bakisTevadoba;
+        private double mandziliErtiLitriSawvavit;
+        public int mgzavrebisRaodenoba;
+        public int gayiduliBiletebi;
+
+    }
+}
